b25232745a49895a99af221c506ed9d222080a82
itorras13 <itorras13@gmail.com>
2016-11-12 16:39:07 -0500

    add slack token security

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, abort
+import os
+
 app = Flask(__name__)
 
 @app.route('/ttt', methods=['POST'])
 def tic_tac_toe():
 	token = request.form.get('token', None)
+	if token != os.environ['slackKey']:
+		return jsonify({'text': 'Wrong token'})
 	text = request.form.get('text', None)
 	return jsonify({
 	 	'text': token + ' ' + text
