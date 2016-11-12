8fb8f9ff3f7b3d3942aaa00377e80a55970f3084
itorras13 <itorras13@gmail.com>
2016-11-12 16:29:47 -0500

    test route for slash command

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/ttt', methods=['POST'])
+def tic_tac_toe():
+	token = request.form.get('token', None)
+	text = request.form.get('text', None)
+	return jsonify({
+	 	'text': token + ' ' + text
+	})
 
 if __name__ == "__main__":
     app.run()
